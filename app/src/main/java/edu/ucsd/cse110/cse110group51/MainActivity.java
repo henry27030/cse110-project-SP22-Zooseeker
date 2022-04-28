397fa318bf6bf412e79cca1a131af1275e0b1ae0
yinshinLi <ysli@ucsd.edu>
2022-04-28 04:06:56 -0700

    try

--- app/src/main/java/edu/ucsd/cse110/cse110group51/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/cse110group51/MainActivity.java
@@ -4,17 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText searchAnimalText;
+    private Button searchButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.searchButton = this.findViewById(R.id.search_btn);
+        this.searchAnimalText = this.findViewById(R.id.searchbar);
+        searchButton.setOnClickListener(this::onSearchClicked);
     }
 
     public void onSearchClicked(View view){
-
+        TextView results = this.findViewById(R.id.results);
+        results.setText("not implemented");
+        String text = searchAnimalText.getText().toString();
+        if(AnimalData.getText()==text){
+            TextView result = this.findViewById(R.id.results);
+            result.setText(text);
+        }
     }
 
     public void onCategoryClicked(View view) {
