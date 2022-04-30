ee7ca1b2f9da9dfc648429c324167d8f94af2f8a
aaron <aaron.lhh0401@gmail.com>
2022-04-29 19:11:13 -0700

    adding list of items

--- app/src/main/java/edu/ucsd/cse110/cse110group51/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/cse110group51/MainActivity.java
@@ -3,32 +3,78 @@ package edu.ucsd.cse110.cse110group51;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+//import androidx.appcompat.widget.SearchView;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText searchAnimalText;
-    private Button searchButton;
+//    private EditText searchAnimalText;
+//    private Button searchButton;
+    private ListView listView;
+
+    private ArrayAdapter<String> arrayAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.searchButton = this.findViewById(R.id.search_btn);
-        this.searchAnimalText = this.findViewById(R.id.searchbar);
-        searchButton.setOnClickListener(this::onSearchClicked);
+//        this.searchButton = this.findViewById(R.id.search_btn);
+//        this.searchAnimalText = this.findViewById(R.id.searchbar);
+        this.listView = this.findViewById(R.id.list_view);
+
+        String[] arr = getResources().getStringArray(R.array.categories);
+        arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                arr);
+        listView.setAdapter(arrayAdapter);
+
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Type here to search");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                arrayAdapter.getFilter().filter(s);
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
     public void onSearchClicked(View view){
-        TextView results = this.findViewById(R.id.results);
-        results.setText("not implemented");
-        String text = searchAnimalText.getText().toString();
-        if(AnimalData.getText()==text){
-            TextView result = this.findViewById(R.id.results);
-            result.setText(text);
-        }
+//        TextView results = this.findViewById(R.id.results);
+//        results.setText("not implemented");
+//        String text = searchAnimalText.getText().toString();
+//        if(AnimalData.getText()==text){
+//            TextView result = this.findViewById(R.id.results);
+//            result.setText(text);
+//        }
     }
 
     public void onCategoryClicked(View view) {
