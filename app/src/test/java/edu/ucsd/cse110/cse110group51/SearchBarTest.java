c25db18ce378fa687599ad099216a952ea306d59
aaron <aaron.lhh0401@gmail.com>
2022-05-01 23:48:50 -0700

    add searchFilterTest
    check if search filter works

--- app/src/test/java/edu/ucsd/cse110/cse110group51/SearchBarTest.java
+++ app/src/test/java/edu/ucsd/cse110/cse110group51/SearchBarTest.java
@@ -1,36 +1,58 @@
 package edu.ucsd.cse110.cse110group51;
 
-        import static org.junit.Assert.assertEquals;
-        import static org.junit.Assert.assertNotEquals;
-        import static org.junit.Assert.assertNotNull;
-        import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
-        import android.content.Context;
+import android.content.Context;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
 
-        import androidx.room.Room;
-        import androidx.test.core.app.ApplicationProvider;
-        import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.lifecycle.Lifecycle;
+import androidx.room.Room;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-        import org.junit.After;
-        import org.junit.Before;
-        import org.junit.Test;
-        import org.junit.runner.RunWith;
-        import org.junit.runner.manipulation.Ordering;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runner.manipulation.Ordering;
 
-        import java.io.IOException;
+import java.io.IOException;
 
 @RunWith(AndroidJUnit4.class)
 public class SearchBarTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> scenarioRule = new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
-    public void testSearch() {
-        /*TodoListItem item1 = new TodoListItem("Pizza time", false, 0);
-        TodoListItem item2 = new TodoListItem("Photos of Spider-Man", false, 1);
+    public void searchFilterTest() {
+        // This is an INTEGRATION test, as we're testing multiple units!
+        // This test SHOULD fail. You need to fix it as an exercise!
 
-        long id1 = dao.insert(item1);
-        long id2 = dao.insert(item2);*/
+        ActivityScenario<MainActivity> scenario = scenarioRule.getScenario();
 
-        assertEquals(true, true);
+        scenario.moveToState(Lifecycle.State.CREATED);
+
+        scenario.onActivity(activity -> {
+            ListView listView = activity.findViewById(R.id.list_view);
+//            MenuItem menuItem = activity.findViewById(R.id.action_search);
+            SearchView searchView = activity.findViewById(R.id.action_search);
+            searchView.setQuery("cat", true);
+
+            int count = listView.getAdapter().getCount();
+            assertEquals(1, count);             // should only have a cat
+        });
     }
 }
 
