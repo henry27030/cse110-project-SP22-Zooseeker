8022bf2a41779aaa95e87728d207293d729a33d6
aaron <aaron.lhh0401@gmail.com>
2022-05-07 22:54:48 -0700

    put back CheckBox
    What I am thinking is that, I will use the same viewModel to do exhibitViewsList and exhibitList. But the difference is that, in exhibitList, checkbox is invisible, in exhibitViewsList, delete-button is invisible

--- app/src/main/java/edu/ucsd/cse110/cse110group51/TodoListAdapter.java
+++ app/src/main/java/edu/ucsd/cse110/cse110group51/TodoListAdapter.java
@@ -52,16 +52,22 @@ public class TodoListAdapter extends RecyclerView.Adapter<TodoListAdapter.ViewHo
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         private final TextView textView;
-        private final TextView text;
+        private final TextView deleted;
+        private final CheckBox checkBox;
 
         private TodoListItem todoItem;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             this.textView=itemView.findViewById(R.id.todo_item_text);
-            this.text=itemView.findViewById(R.id.delete_btn);
+            this.deleted=itemView.findViewById(R.id.delete_btn);
+            this.checkBox=itemView.findViewById(R.id.completed);
 
-            this.text.setOnClickListener(view-> {
+            // in exhibitList, checkBox is invisible and deleted is visible
+            this.deleted.setVisibility(View.VISIBLE);
+            this.checkBox.setVisibility(View.INVISIBLE);
+
+            this.deleted.setOnClickListener(view-> {
                 if (onDeleteClicked == null) return;
                 onDeleteClicked.accept(todoItem);
             });
