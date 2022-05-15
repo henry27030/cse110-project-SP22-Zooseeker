1886fb8e36c36a6d9e1ef9201f30613e1707e455
yinshinLi <93513827+yinshinLi@users.noreply.github.com>
2022-05-15 15:22:54 -0700

    tests

--- app/src/test/java/edu/ucsd/cse110/cse110group51/PlanTest.java
+++ app/src/test/java/edu/ucsd/cse110/cse110group51/PlanTest.java
@@ -1,2 +1,37 @@
-package edu.ucsd.cse110.cse110group51;public class PlanTest {
+package edu.ucsd.cse110.cse110group51;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import android.content.Context;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+import androidx.lifecycle.Lifecycle;
+import androidx.room.Room;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runner.manipulation.Ordering;
+
+import java.io.IOException;
+
+@RunWith(AndroidJUnit4.class)
+public class PlanTest {
+
+
 }
