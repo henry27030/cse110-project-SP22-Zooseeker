2a40f80f544ebc752f205f66d008f37063933663
yinshinLi <93513827+yinshinLi@users.noreply.github.com>
2022-05-24 13:33:41 -0700

    tests_adding

--- app/src/test/java/edu/ucsd/cse110/cse110group51/PlanTest.java
+++ app/src/test/java/edu/ucsd/cse110/cse110group51/PlanTest.java
@@ -1,76 +1,67 @@
 package edu.ucsd.cse110.cse110group51;
 
-
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 
-import android.content.Context;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
 
-import androidx.room.Room;
-import androidx.test.core.app.ApplicationProvider;
+import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import org.junit.After;
-import org.junit.Before;
+import com.google.common.base.Supplier;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.io.IOException;
-
 @RunWith(AndroidJUnit4.class)
 public class PlanTest {
-    private TodoListItemDao dao;
-    private TodoDatabase db;
+    @Rule
+    public ActivityScenarioRule<MainActivity> scenarioRule = new ActivityScenarioRule<>(MainActivity.class);
+    //public ActivityScenarioRule<NextActivity> scenarioRule2 = new ActivityScenarioRule<>(NextActivity.class);
 
-    @Before
-    public void createDb() {
-        Context context = ApplicationProvider.getApplicationContext();
-        db = Room.inMemoryDatabaseBuilder(context, TodoDatabase.class)
-                .allowMainThreadQueries()
-                .build();
-        dao = db.todoListItemDao();
-    }
-    @After
-    public void closeDb() throws IOException {
-        db.close();
-    }
+
 
     @Test
-    public void PlanTest1() {
-        TodoListItem item1 = new TodoListItem( "Pizza time", 0);
-        TodoListItem item2 = new TodoListItem( "Photos of Spider-Man", 1);
+    public void PathFoundSuccessful() {
 
-        long id1 = dao.insert(item1);
-        long id2 = dao.insert(item2);
+        ActivityScenario<MainActivity> scenario = scenarioRule.getScenario();
+        scenario.moveToState(Lifecycle.State.CREATED);
+        //ActivityScenario<NextActivity> scenario2 = scenarioRule2.getScenario();
 
-        // check that these have all been inserted with unique IDs.
-        assertNotEquals(id1, id2);
-    }
 
-    @Test
-    public void PlanTest2() {
-        TodoListItem insertedItem = new TodoListItem( "Pizza time", 0);
-        long id = dao.insert(insertedItem);
+        scenario.onActivity(activity -> {
+            ListView listView = activity.findViewById(R.id.directions_view);
+            int s = PlanActivity.BIND_AUTO_CREATE;
+            Supplier<Class<NextActivity>> classSupplier = () -> NextActivity.class;
+            {
+            }
+            ListView directionsView = activity.findViewById(R.id.list_view);
 
-        TodoListItem item = dao.get(id);
-        assertEquals(id, item.id);
-        assertEquals(insertedItem.text, item.text);
-        assertEquals(insertedItem.order, item.order);
-    }
+            int x = directionsView.getAdapter().getCount();
 
-    @Test
-    public void PlanTest3() {
-        TodoListItem item = new TodoListItem("Pizza time", 0);
-        long id = dao.insert(item);
+            //ListView listView1 = activity.findViewById(R.id.list_view);
+            Supplier<Class<PlanActivity>> classSupplier2 = () -> PlanActivity.class;
+            {
+                int w = PlanActivity.RECEIVER_VISIBLE_TO_INSTANT_APPS;
+            }
 
-        item = dao.get(id);
-        int itemsDeleted = dao.delete(item);
-        assertEquals(1, itemsDeleted);
-        assertNull(dao.get(id));
+            //TextView textView = activity.findViewById(R.id.next_next_btn);
+            //button.performClick();
+            //ListView directionsView = activity.findViewById(R.id.directions_view);
+
+            assertEquals(directionsView.getAdapter().getCount(), x);
+
+            //int count = listView.getAdapter().getCount();
+        });
+
     }
 }
+
 
 
 
--- app/src/test/java/edu/ucsd/cse110/cse110group51/SearchBarTest.java
+++ app/src/test/java/edu/ucsd/cse110/cse110group51/SearchBarTest.java
@@ -26,10 +26,11 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runner.manipulation.Ordering;
-
-import java.io.IOException;*/
+import org.junit.runner.manipulation.Ordering;*/
 /*
+
+import java.io.IOException;
+
 @RunWith(AndroidJUnit4.class)
 public class SearchBarTest {
     @Rule
@@ -43,15 +44,14 @@ public class SearchBarTest {
         scenario.moveToState(Lifecycle.State.CREATED);
 
         scenario.onActivity(activity -> {
-            //ListView listView = activity.findViewById(R.id.list_view);
+            ListView listView = activity.findViewById(R.id.list_view);
 //          MenuItem menuItem = activity.findViewById(R.id.action_search);
-            //SearchView searchView = activity.findViewById(R.id.action_search);
-            //searchView.setQuery("cat", true);
+            SearchView searchView = activity.findViewById(R.id.action_search);
+            searchView.setQuery("cat", true);
 
-            //int count = listView.getAdapter().getCount();
-            assertEquals(1, 1);             // should only have a cat
+            int count = listView.getAdapter().getCount();
+            assertEquals(1, count);             // should only have a cat
         });
-        assertEquals(1, 1);
     }
 
     public void searchCompleteFilterTest() {
@@ -67,9 +67,8 @@ public class SearchBarTest {
             searchView.setQuery("cat", true);
 
             int count = listView.getAdapter().getCount();
-            assertEquals(1, 1);             // should only have a cat
+            assertEquals(1, count);             // should only have a cat
         });
-        assertEquals(1, 1);
     }
 
     public void searchInvalidFilterTest() {
@@ -87,93 +86,6 @@ public class SearchBarTest {
             int count = listView.getAdapter().getCount();
             assertEquals(0, count);             // should only have a cat
         });
-        assertEquals(1, 1);
     }
 }*/
-
-
-
-
-
-
-
-
-
-
-package edu.ucsd.cse110.cse110group51;
-
-        import static org.junit.Assert.assertEquals;
-        import static org.junit.Assert.assertNotEquals;
-        import static org.junit.Assert.assertNotNull;
-        import static org.junit.Assert.assertNull;
-
-        import android.content.Context;
-
-        import androidx.room.Room;
-        import androidx.test.core.app.ApplicationProvider;
-        import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-        import org.junit.After;
-        import org.junit.Before;
-        import org.junit.Test;
-        import org.junit.runner.RunWith;
-
-        import java.io.IOException;
-
-@RunWith(AndroidJUnit4.class)
-public class SearchBarTest {
-    private TodoListItemDao dao;
-    private TodoDatabase db;
-
-    @Before
-    public void createDb() {
-        Context context = ApplicationProvider.getApplicationContext();
-        db = Room.inMemoryDatabaseBuilder(context, TodoDatabase.class)
-                .allowMainThreadQueries()
-                .build();
-        dao = db.todoListItemDao();
-    }
-    @After
-    public void closeDb() throws IOException {
-        db.close();
-    }
-
-    @Test
-    public void SearchFilterTest() {
-        TodoListItem item1 = new TodoListItem( "Pizza time", 0);
-        TodoListItem item2 = new TodoListItem( "Photos of Spider-Man", 1);
-
-        long id1 = dao.insert(item1);
-        long id2 = dao.insert(item2);
-
-        // check that these have all been inserted with unique IDs.
-        assertNotEquals(id1, id2);
-    }
-
-    @Test
-    public void SearchValidTest() {
-        TodoListItem insertedItem = new TodoListItem( "Pizza time", 0);
-        long id = dao.insert(insertedItem);
-
-        TodoListItem item = dao.get(id);
-        assertEquals(id, item.id);
-        assertEquals(insertedItem.text, item.text);
-        assertEquals(insertedItem.order, item.order);
-    }
-
-    @Test
-    public void SearchInvalidTest() {
-        TodoListItem item = new TodoListItem("Pizza time", 0);
-        long id = dao.insert(item);
-
-        item = dao.get(id);
-        int itemsDeleted = dao.delete(item);
-        assertEquals(1, itemsDeleted);
-        assertNull(dao.get(id));
-    }
-}
-
-
-
-
 
