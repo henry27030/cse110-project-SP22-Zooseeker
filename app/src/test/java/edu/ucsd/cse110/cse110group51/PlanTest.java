a4fa97acbcaad5a8cfeb746858b46cab75569247
yinshinLi <93513827+yinshinLi@users.noreply.github.com>
2022-05-24 13:54:56 -0700

    tests_adding

--- app/src/test/java/edu/ucsd/cse110/cse110group51/PlanTest.java
+++ app/src/test/java/edu/ucsd/cse110/cse110group51/PlanTest.java
@@ -22,12 +22,9 @@ import org.junit.runner.RunWith;
 public class PlanTest {
     @Rule
     public ActivityScenarioRule<MainActivity> scenarioRule = new ActivityScenarioRule<>(MainActivity.class);
-    //public ActivityScenarioRule<NextActivity> scenarioRule2 = new ActivityScenarioRule<>(NextActivity.class);
-
-
 
     @Test
-    public void PathPlannedSuccessful() {
+    public void PlanSuccessful() {
 
         ActivityScenario<MainActivity> scenario = scenarioRule.getScenario();
         scenario.moveToState(Lifecycle.State.CREATED);
@@ -37,7 +34,7 @@ public class PlanTest {
         scenario.onActivity(activity -> {
             ListView listView = activity.findViewById(R.id.directions_view);
             int s = PlanActivity.BIND_AUTO_CREATE;
-            Supplier<Class<NextActivity>> classSupplier = () -> NextActivity.class;
+            Supplier<Class<PlanActivity>> classSupplier = () -> PlanActivity.class;
             {
             }
             ListView directionsView = activity.findViewById(R.id.list_view);
@@ -61,101 +58,3 @@ public class PlanTest {
 
     }
 }
-
-
-/*package edu.ucsd.cse110.cse110group51;
-
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-
-import org.jgrapht.GraphPath;
-import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
-
-import androidx.lifecycle.Lifecycle;
-import androidx.test.core.app.ActivityScenario;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import java.util.ArrayList;
-import java.util.concurrent.atomic.AtomicReference;*/
-
-/*
-@RunWith(AndroidJUnit4.class)
-public class PlanTest {
-    @Rule
-    public ActivityScenarioRule<MainActivity> scenarioRule = new ActivityScenarioRule<>(MainActivity.class);
-
-    @Test
-    public void PlanInitializationTest() {
-        ActivityScenario<MainActivity> scenario = scenarioRule.getScenario();
-
-        scenario.moveToState(Lifecycle.State.CREATED);
-
-        scenario.onActivity(activity -> {
-            //ListView listView = activity.findViewById(R.id.directions_view);
-            //assertEquals(listView.getAdapter().getCount(), 0);
-            assertEquals(0,0);
-        });
-    }
-*/
-    /*@Test
-    public void SinglePlanTest() {
-        ActivityScenario<MainActivity> scenario = scenarioRule.getScenario();
-
-        scenario.moveToState(Lifecycle.State.CREATED);
-
-        scenario.onActivity(activity -> {
-            //ListView listView = activity.findViewById(R.id.directions_view);
-            //assertEquals(listView.getAdapter().getCount(), 0);
-            assertEquals(0,0);
-        });
-    }
-
-    @Test
-    public void SinglePlanTest2() {
-        //Graph<String, IdentifiedWeightedEdge> g;
-        //AtomicReference<GraphPath<String, IdentifiedWeightedEdge>> path = null;
-        ActivityScenario<MainActivity> scenario = scenarioRule.getScenario();
-
-        scenario.moveToState(Lifecycle.State.CREATED);
-
-        scenario.onActivity(activity -> {
-            //Context context = getApplication().getApplicationContext();
-
-            // 1. Load the graph...
-            //g = ZooData.loadZooGraphJSON(context, "sample_zoo_graph.json");
-            //GraphPath<String, IdentifiedWeightedEdge> path = DijkstraShortestPath.findPathBetween(g, start, goal);
-
-            // 2. Load the information about our nodes and edges...
-            //vInfo = ZooData.loadVertexInfoJSON(context, "sample_node_info.json");
-            //eInfo = ZooData.loadEdgeInfoJSON(context, "sample_edge_info.json");
-            //ArrayAdapter<String> arrayAdapter = null;
-            int count = 0;
-            //ListView directionsView = activity.findViewById(R.id.action_search);
-            //directionsView.getAdapter();
-            //MainActivity.exhibitList.add("lions");
-            //ArrayList<String> Directions = new ArrayList<String>();
-            //ArrayList<String> exhibitListInFunc = new ArrayList<String>();
-            //exhibitListInFunc.add("lions");
-            //String start = "entrance_exit_gate";
-            //int ShortestPath = 0;
-            //path.set(DijkstraShortestPath.findPathBetween(MainActivity.g, start, "lions"));
-            //MainActivity.Directions = Directions;
-
-            //arrayAdapter = new ArrayAdapter<String>;
-            //directionsView.setAdapter(arrayAdapter);
-
-            assertEquals(0, count);
-        });
-    }*/
-//}
