b0a3eb8ac5108a29ae0958e562d510dcd67c15dc
yinshinLi <93513827+yinshinLi@users.noreply.github.com>
2022-05-24 14:12:34 -0700

    tests_adding

--- app/src/test/java/edu/ucsd/cse110/cse110group51/PlanTest.java
+++ app/src/test/java/edu/ucsd/cse110/cse110group51/PlanTest.java
@@ -1,51 +1,13 @@
 package edu.ucsd.cse110.cse110group51;
 
-import static org.junit.Assert.assertEquals;
-
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.SearchView;
-import android.widget.TextView;
-
-import androidx.lifecycle.Lifecycle;
-import androidx.test.core.app.ActivityScenario;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import com.google.common.base.Supplier;
-
-import org.junit.After;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-import java.lang.reflect.Field;
+import static org.junit.Assert.*;
 
-@RunWith(AndroidJUnit4.class)
+
 public class PlanTest {
-    @Rule
-    public ActivityScenarioRule<MainActivity> scenarioRule = new ActivityScenarioRule<>(MainActivity.class);
-/*
-    @After
-    public void finishComponentTesting() {
-        // sInstance is the static variable name which holds the singleton instance
-        resetSingleton(MainActivity.class, "sInstance");
-    }
-
-    private void resetSingleton(Class clazz, String fieldName) {
-        Field instance;
-        try {
-            instance = clazz.getDeclaredField(fieldName);
-            instance.setAccessible(true);
-            instance.set(null, null);
-        } catch (Exception e) {
-            throw new RuntimeException();
-        }
-    }
-*/
     @Test
-    public void PlanSuccessful() {
-        assertEquals(0,0);
-
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
     }
 }
