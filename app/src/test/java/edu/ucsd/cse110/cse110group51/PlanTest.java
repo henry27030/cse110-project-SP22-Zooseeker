b053e4d9ff43e9900796962e098c9ffd338d3b18
yinshinLi <93513827+yinshinLi@users.noreply.github.com>
2022-05-24 13:43:17 -0700

    tests_adding

--- app/src/test/java/edu/ucsd/cse110/cse110group51/PlanTest.java
+++ app/src/test/java/edu/ucsd/cse110/cse110group51/PlanTest.java
@@ -1,72 +1,5 @@
 package edu.ucsd.cse110.cse110group51;
 
-import static org.junit.Assert.assertEquals;
-
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.SearchView;
-import android.widget.TextView;
-
-import androidx.lifecycle.Lifecycle;
-import androidx.test.core.app.ActivityScenario;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import com.google.common.base.Supplier;
-
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-@RunWith(AndroidJUnit4.class)
-public class PlanTest {
-    @Rule
-    public ActivityScenarioRule<MainActivity> scenarioRule = new ActivityScenarioRule<>(MainActivity.class);
-    //public ActivityScenarioRule<NextActivity> scenarioRule2 = new ActivityScenarioRule<>(NextActivity.class);
-
-
-
-    @Test
-    public void PathFoundSuccessful() {
-
-        ActivityScenario<MainActivity> scenario = scenarioRule.getScenario();
-        scenario.moveToState(Lifecycle.State.CREATED);
-        //ActivityScenario<NextActivity> scenario2 = scenarioRule2.getScenario();
-
-
-        scenario.onActivity(activity -> {
-            ListView listView = activity.findViewById(R.id.directions_view);
-            int s = PlanActivity.BIND_AUTO_CREATE;
-            Supplier<Class<NextActivity>> classSupplier = () -> NextActivity.class;
-            {
-            }
-            ListView directionsView = activity.findViewById(R.id.list_view);
-
-            int x = directionsView.getAdapter().getCount();
-
-            //ListView listView1 = activity.findViewById(R.id.list_view);
-            Supplier<Class<PlanActivity>> classSupplier2 = () -> PlanActivity.class;
-            {
-                int w = PlanActivity.RECEIVER_VISIBLE_TO_INSTANT_APPS;
-            }
-
-            //TextView textView = activity.findViewById(R.id.next_next_btn);
-            //button.performClick();
-            //ListView directionsView = activity.findViewById(R.id.directions_view);
-
-            assertEquals(directionsView.getAdapter().getCount(), x);
-
-            //int count = listView.getAdapter().getCount();
-        });
-
-    }
-}
-
-
-
-
-/*package edu.ucsd.cse110.cse110group51;
-
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -89,9 +22,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
-import java.util.concurrent.atomic.AtomicReference;*/
+import java.util.concurrent.atomic.AtomicReference;
 
-/*
+
 @RunWith(AndroidJUnit4.class)
 public class PlanTest {
     @Rule
@@ -110,8 +43,21 @@ public class PlanTest {
         });
     }
 
-    @Test
+    /*@Test
     public void SinglePlanTest() {
+        ActivityScenario<MainActivity> scenario = scenarioRule.getScenario();
+
+        scenario.moveToState(Lifecycle.State.CREATED);
+
+        scenario.onActivity(activity -> {
+            //ListView listView = activity.findViewById(R.id.directions_view);
+            //assertEquals(listView.getAdapter().getCount(), 0);
+            assertEquals(0,0);
+        });
+    }
+
+    @Test
+    public void SinglePlanTest2() {
         //Graph<String, IdentifiedWeightedEdge> g;
         //AtomicReference<GraphPath<String, IdentifiedWeightedEdge>> path = null;
         ActivityScenario<MainActivity> scenario = scenarioRule.getScenario();
@@ -128,16 +74,16 @@ public class PlanTest {
             // 2. Load the information about our nodes and edges...
             //vInfo = ZooData.loadVertexInfoJSON(context, "sample_node_info.json");
             //eInfo = ZooData.loadEdgeInfoJSON(context, "sample_edge_info.json");
-            ArrayAdapter<String> arrayAdapter = null;
+            //ArrayAdapter<String> arrayAdapter = null;
             int count = 0;
             //ListView directionsView = activity.findViewById(R.id.action_search);
             //directionsView.getAdapter();
-            MainActivity.exhibitList.add("lions");
-            ArrayList<String> Directions = new ArrayList<String>();
-            ArrayList<String> exhibitListInFunc = new ArrayList<String>();
-            exhibitListInFunc.add("lions");
-            String start = "entrance_exit_gate";
-            int ShortestPath = 0;
+            //MainActivity.exhibitList.add("lions");
+            //ArrayList<String> Directions = new ArrayList<String>();
+            //ArrayList<String> exhibitListInFunc = new ArrayList<String>();
+            //exhibitListInFunc.add("lions");
+            //String start = "entrance_exit_gate";
+            //int ShortestPath = 0;
             //path.set(DijkstraShortestPath.findPathBetween(MainActivity.g, start, "lions"));
             //MainActivity.Directions = Directions;
 
@@ -146,5 +92,5 @@ public class PlanTest {
 
             assertEquals(0, count);
         });
-    }
-}*/
+    }*/
+}
