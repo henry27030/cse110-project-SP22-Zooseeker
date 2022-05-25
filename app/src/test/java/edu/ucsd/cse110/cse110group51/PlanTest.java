1f7ed0b69e7b3ca9a2a8f4689300ef63b510d3e9
yinshinLi <93513827+yinshinLi@users.noreply.github.com>
2022-05-24 21:06:57 -0700

    tests

--- app/src/test/java/edu/ucsd/cse110/cse110group51/PlanTest.java
+++ app/src/test/java/edu/ucsd/cse110/cse110group51/PlanTest.java
@@ -1,54 +1,54 @@
-package edu.ucsd.cse110.cse110group51;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-
-import android.content.Context;
-import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.SearchView;
-import android.widget.TextView;
-
-import androidx.lifecycle.Lifecycle;
-import androidx.room.Room;
-import androidx.test.core.app.ActivityScenario;
-import androidx.test.core.app.ApplicationProvider;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import org.jgrapht.GraphPath;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runner.manipulation.Ordering;
-
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.concurrent.atomic.AtomicReference;
-
-@RunWith(AndroidJUnit4.class)
-public class PlanTest {
-    @Rule
-    public ActivityScenarioRule<MainActivity> scenarioRule = new ActivityScenarioRule<>(MainActivity.class);
-
-
-    @Test
-    public void PlanInitializationTest() {
-
-    }
-
-    @Test
-    public void SinglePlanTest() {
-
-    }
-
-}
+//package edu.ucsd.cse110.cse110group51;
+//
+//import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertFalse;
+//import static org.junit.Assert.assertNotEquals;
+//import static org.junit.Assert.assertNotNull;
+//import static org.junit.Assert.assertNull;
+//
+//import android.content.Context;
+//import android.view.MenuItem;
+//import android.widget.ArrayAdapter;
+//import android.widget.Button;
+//import android.widget.EditText;
+//import android.widget.ListView;
+//import android.widget.SearchView;
+//import android.widget.TextView;
+//
+//import androidx.lifecycle.Lifecycle;
+//import androidx.room.Room;
+//import androidx.test.core.app.ActivityScenario;
+//import androidx.test.core.app.ApplicationProvider;
+//import androidx.test.ext.junit.rules.ActivityScenarioRule;
+//import androidx.test.ext.junit.runners.AndroidJUnit4;
+//
+//import org.jgrapht.GraphPath;
+//import org.junit.After;
+//import org.junit.Before;
+//import org.junit.Rule;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.junit.runner.manipulation.Ordering;
+//
+//
+//import java.io.IOException;
+//import java.util.ArrayList;
+//import java.util.concurrent.atomic.AtomicReference;
+//
+//@RunWith(AndroidJUnit4.class)
+//public class PlanTest {
+//    @Rule
+//    public ActivityScenarioRule<MainActivity> scenarioRule = new ActivityScenarioRule<>(MainActivity.class);
+//
+//
+//    @Test
+//    public void PlanInitializationTest() {
+//
+//    }
+//
+//    @Test
+//    public void SinglePlanTest() {
+//
+//    }
+//
+//}
