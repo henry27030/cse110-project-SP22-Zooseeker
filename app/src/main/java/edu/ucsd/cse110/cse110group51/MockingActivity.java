654eeefe2668b4b2ccd1a15b7b39dca572fe7a60
yinshinLi <93513827+yinshinLi@users.noreply.github.com>
2022-05-29 04:20:03 -0700

    mocking_not_accept_null

--- app/src/main/java/edu/ucsd/cse110/cse110group51/MockingActivity.java
+++ app/src/main/java/edu/ucsd/cse110/cse110group51/MockingActivity.java
@@ -24,9 +24,11 @@ public class MockingActivity extends AppCompatActivity {
     public void onMockClicked(View view) {
         this.xCoorText = this.findViewById(R.id.x_coor);
         this.yCoorText = this.findViewById(R.id.y_coor);
-        double xCoor   = Double.parseDouble(xCoorText.getText().toString());
-        double yCoor   = Double.parseDouble(yCoorText.getText().toString());
-        MainActivity.UserCoord = Coord.of(xCoor, yCoor);
-        finish();
+        //Check if coordinates are filled
+        if(xCoorText.getText().toString().trim().length() != 0 & yCoorText.getText().toString().trim().length() != 0) {
+            double xCoor = Double.parseDouble(xCoorText.getText().toString());
+            double yCoor = Double.parseDouble(yCoorText.getText().toString());
+            MainActivity.UserCoord = Coord.of(xCoor, yCoor);
+        }
     }
 }
