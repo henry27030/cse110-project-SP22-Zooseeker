de37171c5c5433365c737457ae352b818a494ef4
yinshinLi <93513827+yinshinLi@users.noreply.github.com>
2022-05-31 15:30:32 -0700

    exhibitListTest

--- app/src/test/java/edu/ucsd/cse110/cse110group51/ExhibitListTest.java
+++ app/src/test/java/edu/ucsd/cse110/cse110group51/ExhibitListTest.java
@@ -1,2 +1,91 @@
-package edu.ucsd.cse110.cse110group51;public class ExhibitTest {
+package edu.ucsd.cse110.cse110group51;
+
+import static org.junit.Assert.assertEquals;
+
+import android.arch.core.executor.testing.InstantTaskExecutorRule;
+import android.content.Context;
+import android.widget.Button;
+import android.widget.ListView;
+
+import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.util.List;
+
+@RunWith(AndroidJUnit4.class)
+public class ExhibitListTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> scenarioRuleMain = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Rule
+    public InstantTaskExecutorRule execRule = new InstantTaskExecutorRule();
+
+
+    private TodoDatabase db;
+    private TodoListItemDao dao;
+
+
+    @Before
+    public void createDb() {
+        Context context = ApplicationProvider.getApplicationContext();
+        db = TodoDatabase.getSingleton(context);
+        dao = db.todoListItemDao();
+    }
+
+    @After
+    public void closeDb() throws IOException {
+        db.close();
+    }
+
+    @Test
+    public void AddExhibitTest() {
+        ActivityScenario<MainActivity> scenario = scenarioRuleMain.getScenario();
+        scenario.moveToState(Lifecycle.State.CREATED);
+        scenario.onActivity(activity -> {
+            MainActivity.exhibitList.add("Koi Fish");
+            List<TodoListItem> list = MainActivity.viewModel.getCurrentItems();
+            for (TodoListItem item : list) {
+                if (!MainActivity.exhibitList.contains(item.text)) {
+                    MainActivity.exhibitList.add(item.text);
+                }
+            }
+            Button planButton = activity.findViewById(R.id.plan_btn);
+            planButton.performClick();
+
+            assertEquals(MainActivity.exhibitList.size(),1);
+        });
+
+    }
+
+    @Test
+    public void DeleteExhibitTest() {
+        ActivityScenario<MainActivity> scenario = scenarioRuleMain.getScenario();
+        scenario.moveToState(Lifecycle.State.CREATED);
+        scenario.onActivity(activity -> {
+            //MainActivity.exhibitList.add("Koi Fish");
+            List<TodoListItem> list = MainActivity.viewModel.getCurrentItems();
+            for (TodoListItem item : list) {
+                if (!MainActivity.exhibitList.contains(item.text)) {
+                    MainActivity.exhibitList.add(item.text);
+                }
+            }
+            Button planButton = activity.findViewById(R.id.plan_btn);
+            planButton.performClick();
+
+            MainActivity.exhibitList.remove("Koi Fish");
+            assertEquals(MainActivity.exhibitList.size(),0);
+
+        });
+
+    }
 }
--- app/src/test/java/edu/ucsd/cse110/cse110group51/PlanTest.java
+++ app/src/test/java/edu/ucsd/cse110/cse110group51/PlanTest.java
@@ -72,18 +72,13 @@ public class PlanTest {
         });
     }
 
-//    @Rule
-//    public ActivityScenarioRule<TodoListActivity> scenarioRuleTodo = new ActivityScenarioRule<>(TodoListActivity.class);
-
-
     @Test
     public void SinglePlanTest() {
         ActivityScenario<MainActivity> scenario = scenarioRuleMain.getScenario();
         scenario.moveToState(Lifecycle.State.CREATED);
         scenario.onActivity(activity -> {
             MainActivity.exhibitList.add("Koi Fish");
-//            MainActivity.viewModel = new ViewModelProvider(this.Activity)
-//                    .get(TodoListViewModel.class);
+
             List<TodoListItem> list = MainActivity.viewModel.getCurrentItems();
             for (TodoListItem item : list) {
                 if (!MainActivity.exhibitList.contains(item.text)) {
@@ -93,10 +88,6 @@ public class PlanTest {
             Button planButton = activity.findViewById(R.id.plan_btn);
             planButton.performClick();
 
-//            Button planDisplay = TodoListActivity.findViewById(R.id.plan_display);
-//            planDisplay.performClick();
-
-            //assertEquals(directionsView.getAdapter().getCount(), count);*/
         });
 
         ActivityScenario<PlanActivity> scenario2 = scenarioRulePlan.getScenario();
@@ -112,57 +103,5 @@ public class PlanTest {
 
         });
 
-        //ActivityScenario<TodoListActivity> scenario2 = scenarioRuleTodo.getScenario();
-//        scenario2.moveToState(Lifecycle.State.CREATED);
-//        scenario2.onActivity(activity ->
-
-
-
-//        ActivityScenario<TodoListActivity> scenario3 = scenarioRuleTodo.getScenario();
-//        scenario3.moveToState(Lifecycle.State.CREATED);
-//        scenario3.onActivity(activity -> {
-//            Button planDisplay = activity.findViewById(R.id.plan_display);
-//            planDisplay.performClick();
-//        });
-
-
-        //ActivityScenario.close;
-
-//        ActivityScenario<TodoListActivity> scenario2 = scenarioRuleTodo.getScenario();
-//        scenario2.moveToState(Lifecycle.State.CREATED);
-//        scenario2.onActivity(activity -> {
-////            MainActivity.exhibitList.add("Koi Fish");
-////            Button planButton = activity.findViewById(R.id.plan_btn);
-////            planButton.performClick();
-//            Button planDisplay = activity.findViewById(R.id.plan_display);
-//            planDisplay.performClick();
-//        });
     }
-
-//    @Test
-//    public void PlanTest2() {
-//        ActivityScenario<PlanActivity> scenario = scenarioRulePlan.getScenario();
-//
-//        scenario.moveToState(Lifecycle.State.CREATED);
-//
-//        scenario.onActivity(activity -> {
-//            ListView listView = activity.findViewById(R.id.directions_view);
-//            assertEquals(listView.getAdapter().getCount(), 0);
-//
-//        });
-//    }
-
-
-//    @Rule
-//    ActivityScenarioRule<TodoListActivity> scenarioRuleTodo = new ActivityScenarioRule<>(TodoListActivity.class);
-//
-//    @Test
-//    public void SinPlanTest() {
-//        ActivityScenario<MainActivity> scenario = scenarioRuleMain.getScenario();
-//        scenario.moveToState(Lifecycle.State.CREATED);
-//        scenario.onActivity(activity -> {
-//            Button planDisplay = activity.findViewById(R.id.plan_display);
-//            planDisplay.performClick();
-//        });
-//    }
 }
